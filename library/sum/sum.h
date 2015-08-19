ea755e1c1e3c49f6d1ade87724ba4b4c65fa7a02
Dmitry Kuznetsov <smith.dmit@gmail.com>
2015-08-19 20:06:34 +0200

    test projects layout

---
+++ library/sum/sum.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+template <typename T>
+T sum(T a, T b) {
+    return a + b;
+}
+
---
+++ projects/summator/main.cpp
@@ -0,0 +1,9 @@
+#include <library/sum/sum.h>
+
+#include <iostream>
+
+
+int main() {
+    std::cout << sum(3, 2) << std::endl;
+}
+
